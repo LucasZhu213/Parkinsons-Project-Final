1ac7bab15c9e4ea7f41a8113718c3307614b6024
LucasZhu213 <123782273+LucasZhu213@users.noreply.github.com>
2026-05-13 15:34:05 -0400

    Fixed 3d CNN training

--- pretrained3DCNN.py
+++ pretrained3DCNN.py
@@ -1,6 +1,6 @@
 from classification_models_3D.kkeras import Classifiers
 import numpy as np
-import random
+import random, os, re, pandas as pd
 import matplotlib.pyplot as plt
 from skimage import measure
 from keras.optimizers import Adam
@@ -9,8 +9,10 @@ from keras import backend as K
 from keras.layers import Dropout, Dense, Activation, GlobalAveragePooling3D
 from keras.models import Model
 from keras.src.utils import summary_utils
+from sklearn.model_selection import train_test_split
+from scipy.ndimage import rotate
 use_weights = 'imagenet'
-shape_size = (96, 96, 96, 3)
+shape_size = (96, 128, 128, 3)
 backbone = 'resnet18'
 num_classes = 2
 batch_size_train = 12
@@ -21,6 +23,28 @@ epochs = 50
 steps_per_epoch = 100
 validation_steps = 20
 dropout_val = 0.1
+labels = pd.read_csv("LABELS.csv").set_index("Subject")
+labels = labels[~labels.index.duplicated(keep='first')]
+random.seed(42)
+np.random.seed(42)
+
+models, answers = [], []
+for file in os.listdir("3D_VOLUMES"):
+    subjnum = re.search("^\d+", file).group()
+    models.append(np.load(file))
+    answers.append({"Control":0, "Prodromal":1, "PD":2}[labels.loc[subjnum]["Group"]])
+    
+X_train, X_val, y_train, y_val = train_test_split(models, answers, test_size=0.25, random_state=42, stratify=answers)
+
+def augment(volume, max_rotation=10):
+    if random.random() < 0.5:
+        angle = np.random.uniform(-max_rotation, max_rotation)
+        volume = rotate(volume, angle, axes=(1, 2), reshape=False, order=1, mode='nearest')
+    if random.random() < 0.5:
+        volume *= np.random.uniform(0.9, 1.1)
+    volume += np.random.normal(0, 0.01, volume.shape)
+    volume = (volume - np.mean(volume)) / (np.std(volume)+ 1e-8)
+    return volume
 
 def get_model_memory_usage(batch_size, model):
     shapes_mem_count = 0
@@ -52,121 +76,20 @@ def get_model_memory_usage(batch_size, model):
     gbytes = np.round(total_memory / (1024.0 ** 3), 3) + internal_model_mem_count
     return gbytes
 
-def sphere(shape, radius, position):
-    """Generate an n-dimensional spherical mask."""
-    # assume shape and position have the same length and contain ints
-    # the units are pixels / voxels (px for short)
-    # radius is a int or float in px
-    assert len(position) == len(shape)
-    n = len(shape)
-    semisizes = (radius,) * len(shape)
-
-    # genereate the grid for the support points
-    # centered at the position indicated by position
-    grid = [slice(-x0, dim - x0) for x0, dim in zip(position, shape)]
-    position = np.ogrid[grid]
-    # calculate the distance of all points from `position` center
-    # scaled by the radius
-    arr = np.zeros(shape, dtype=float)
-    for x_i, semisize in zip(position, semisizes):
-        # this can be generalized for exponent != 2
-        # in which case `(x_i / semisize)`
-        # would become `np.abs(x_i / semisize)`
-        arr += (x_i / semisize) ** 2
-
-    # the inner part of the sphere will have distance below or equal to 1
-    return arr <= 1.0
-
-def gen_random_volume(debug=False):
-    img = np.zeros((96, 96, 96, 3), dtype=np.uint8)
-    answ = np.array([0, 0])
-    num_sheres = random.randint(2, 3)
-    min_radius = 3
-    max_radius = 20
-    num_cubes = random.randint(2, 3)
-    min_cube_side = 3
-    max_cube_side = 15
-
-    # Background
-    dark_color0 = random.randint(0, 100)
-    dark_color1 = random.randint(0, 100)
-    dark_color2 = random.randint(0, 100)
-    img[..., 0] = dark_color0
-    img[..., 1] = dark_color1
-    img[..., 2] = dark_color2
-
-    if random.uniform(0, 1) > 0.5:
-        # Spheres
-        for i in range(num_sheres):
-            light_color0 = random.randint(dark_color0+1, 255)
-            light_color1 = random.randint(dark_color1+1, 255)
-            light_color2 = random.randint(dark_color2+1, 255)
-            center_0 = random.randint(0, img.shape[0] - 1)
-            center_1 = random.randint(0, img.shape[1] - 1)
-            center_2 = random.randint(0, img.shape[2] - 1)
-            r1 = random.randint(min_radius, max_radius)
-            # print(r1, (center_0, center_1, center_2), (light_color0, light_color1, light_color2))
-            s = sphere(img.shape[:-1], r1, (center_0, center_1, center_2))
-            tmp = img.copy()
-            tmp[s] = (light_color0, light_color1, light_color2)
-            img[s] = tmp[s]
-            # print(img.min(), img.max(), img.mean(), img.dtype)
-        answ[0] = 1
-
-    if random.uniform(0, 1) > 0.5:
-        # Cubes
-        for i in range(num_cubes):
-            light_color0 = random.randint(dark_color0 + 1, 255)
-            light_color1 = random.randint(dark_color1 + 1, 255)
-            light_color2 = random.randint(dark_color2 + 1, 255)
-            range0_start = random.randint(0, img.shape[0] - max_cube_side)
-            range0_end = range0_start + random.randint(min_cube_side, max_cube_side)
-            range1_start = random.randint(0, img.shape[1] - max_cube_side)
-            range1_end = range1_start + random.randint(min_cube_side, max_cube_side)
-            range2_start = random.randint(0, img.shape[2] - max_cube_side)
-            range2_end = range2_start + random.randint(min_cube_side, max_cube_side)
-            img[range0_start:range0_end, range1_start:range1_end, range2_start:range2_end] = (light_color0, light_color1, light_color2)
-        answ[1] = 1
-
-    # Debug
-    if debug:
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1, projection='3d')
-
-        verts, faces, normals, values = measure.marching_cubes(img[..., 1], 127)
-        ax.plot_trisurf(
-            verts[:, 0],
-            verts[:, 1],
-            faces,
-            verts[:, 2],
-            cmap='Spectral',
-            antialiased=False,
-            linewidth=0.0
-        )
-        plt.show()
-
-    # White noise
-    density = random.uniform(0, 0.1)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            for k in range(img.shape[2]):
-                if random.random() < density:
-                    img[i, j, k] = (
-                        random.randint(0, 255),
-                        random.randint(0, 255),
-                        random.randint(0, 255),
-                    )
-
-    return img, answ
-
-def batch_generator(batch_size, preprocess_input):
+def batch_generator(batch_size, models, answers, preprocess_input):
+    iterator = 0
     while True:
         image_list = []
         answ_list = []
-        for i in range(batch_size):
-            img, answ = gen_random_volume()
-            image_list.append(img)
-            answ_list.append(answ)
+        if iterator+batch_size < len(models):
+            for i in range(iterator, iterator+batch_size):
+                img, answ = models[i], answers[i]
+                image_list.append(img); answ_list.append(answ)
+        else:
+            for i in range(iterator, len(models)):
+                img, answ = models[i], answers[i]
+                image_list.append(img); answ_list.append(answ)
+            iterator = 0
 
         image_list = np.array(image_list, dtype=np.float32)
         image_list = preprocess_input(image_list)
@@ -205,12 +128,14 @@ callbacks = [
 ]
 
 gen_train = batch_generator(
-        batch_size_train,
-        preprocess_input
-    )
+    batch_size_train,
+    X_train, y_train,
+    preprocess_input
+)
 gen_valid = batch_generator(
     batch_size_valid,
-    preprocess_input,
+    X_val, y_val,
+    preprocess_input
 )
 history = model.fit(
     gen_train,
